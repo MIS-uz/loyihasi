1a9817249700ae99fb46095f2ff06825f5937c2b
MIS-uz <jbahodir770@gmail.com>
2025-12-22 16:38:30 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu(){
-  document.getElementById("menu").classList.toggle("active");
-}
-
-window.addEventListener("scroll",()=>{
-  document.getElementById("header")
-    .classList.toggle("shrink",window.scrollY>80);
-});
-
